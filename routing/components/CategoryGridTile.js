dbd8632f2ffa0f81415df3b12b388a6c1c2e8d7b
glen-s-abraham <glenprojects666@gmail.com>
2023-08-05 11:59:57 +0530

    basic home screen

---
+++ routing/components/CategoryGridTile.js
@@ -0,0 +1,42 @@
+import { Pressable, StyleSheet, Text, View } from "react-native";
+
+export default function CategoryGridTile({ title, color }) {
+    return (
+        <View style={styles.gridItem}>
+            <Pressable android_ripple={{color: '#ccc' }} style={({ pressed }) => [styles.button, pressed ? styles.buttonPressed : null]}>
+                <View style={[styles.innerContainer,{backgroundColor:color}]}>
+                    <Text style={styles.title}>{title}</Text>
+                </View>
+            </Pressable>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    gridItem: {
+        flex: 1,
+        margin: 16,
+        height: 150,
+        borderRadius: 8,
+        elevation: 2,
+        overflow: 'hidden'
+    },
+    button: {
+        flex: 1
+    },
+    buttonPressed: {
+        opacity: 0.5
+    },
+    innerContainer: {
+        flex: 1,
+        padding: 16,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius:8
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 18,
+
+    }
+})
