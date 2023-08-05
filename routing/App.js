d813aef50639556b638396cfab30ac2d04b680c4
glen-s-abraham <glenprojects666@gmail.com>
2023-08-05 12:57:16 +0530

    basic native stack navigator added

--- routing/App.js
+++ routing/App.js
@@ -1,10 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import CategoriesScreen from './screens/CategoriesScreen';
+
+
+
+const stack = createNativeStackNavigator();
+
 
 export default function App() {
   return (
-   <CategoriesScreen/>
+    <NavigationContainer>
+      <stack.Navigator>
+        <stack.Screen name='MealsCategories' component={CategoriesScreen}/>
+      </stack.Navigator>
+    </NavigationContainer>
   );
 }
 
